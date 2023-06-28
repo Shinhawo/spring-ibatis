d687549c090836d9fa3bddcba3153cfd9cfbcfce
신하원 <hownu080@naver.com>
2023-06-28 17:19:48 +0900

    iBatis 실습

---
+++ src/main/java/vo/Employee.java
@@ -0,0 +1,27 @@
+package vo;
+
+public class Employee {
+
+	private int id;
+	private String firstName;
+	
+	public Employee() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+}
